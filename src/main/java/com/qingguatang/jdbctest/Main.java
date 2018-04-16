3b8463f27a423f18eeb9f633de910db3b799953e
zhouxue <zhouxue@qingguatang.com>
2018-04-16 18:31:51 +0800

    完成JDBC第一次连接，创建一张用于测试用的user表

--- src/main/java/com/qingguatang/jdbctest/Main.java
+++ src/main/java/com/qingguatang/jdbctest/Main.java
@@ -1,15 +1,8 @@
 package com.qingguatang.jdbctest;
 
-import com.qingguatang.jdbctest.dao.api.UserDAO;
-import com.qingguatang.jdbctest.dao.impl.UserDAOImpl;
-import com.qingguatang.jdbctest.dao.model.UserDO;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
 
 /**
  * Main的描述:<br>
@@ -19,10 +12,36 @@ import java.util.Properties;
 public class Main {
 
   public static void main(String[] args){
-    UserDAO userDAO = new UserDAOImpl();
-    UserDO userDO = userDAO.selectById(4);
+    try {
+      // 1. 加载一个mysql的驱动，这里封装了对mysql数据库操作的具体实现
+      Class.forName("com.mysql.jdbc.Driver");
 
-    System.out.println(userDO.toString());
+      // 2. 配置mysql数据库的访问地址，以及用户名及密码
+      String dbURL = "jdbc:mysql://localhost:3306/test";
+      String userName = "root";
+      String password = "";
+
+      // 3. 创建与数据库中的连接通道
+      Connection connection = DriverManager.getConnection(dbURL, userName, password);
+
+      // 4. 使用JDBC的方式创建一个SQL
+      String createUserTableSQL = "CREATE TABLE `user1` (\n"
+          + "  `id` int(11) NOT NULL AUTO_INCREMENT,\n"
+          + "  `name` varchar(45) DEFAULT NULL,\n"
+          + "  PRIMARY KEY (`id`)\n"
+          + ") ";
+      Statement statement = connection.createStatement();
+
+      // 5. 执行SQL，并且封装了DB中的返回
+      int result = statement.executeUpdate(createUserTableSQL);
+
+      // 6. 关闭创建的客户端与DB的连接
+      statement.close();
+      connection.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
   }
 
 }
